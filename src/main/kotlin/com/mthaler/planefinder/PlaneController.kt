97791ca90318590e0b682846a432f9794853f6ed
Michael Thaler <michael.thaler@dlr.de>
2021-06-13 09:49:49 +0200

    Implemented PlaneController.

--- src/main/kotlin/com/mthaler/planefinder/PlaneController.kt
+++ src/main/kotlin/com/mthaler/planefinder/PlaneController.kt
@@ -1,7 +1,17 @@
 package com.mthaler.planefinder
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.io.IOException
 
 @Controller
-class PlaneController {
+class PlaneController(private val pfService: PlaneFinderService) {
+
+    @ResponseBody
+    @GetMapping("/aircraft")
+    @Throws(IOException::class)
+    fun getCurrentAircraft(): Iterable<Aircraft?>? {
+        return pfService.getAircraft()
+    }
 }
